96159c302b3a557cd7a25d7458080c53a8fb033a
dante-signal31 <dante.signal31@gmail.com>
2020-02-16 14:00:27 +0100

    Added pytest ficture to create temporal folder at /tmp.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages  # Always prefer setuptools over distutilss
 
-VERSION = "1.0.2"
+VERSION = "1.1"
 
 with open("README.md") as readme:
     LONG_DESCRIPTION = readme.read()
