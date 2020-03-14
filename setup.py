1b0f9b95cb4cfe969dc0045f60d9e5de3e8f4658
dante-signal31 <dante.signal31@gmail.com>
2020-03-14 01:24:20 +0100

    Added funtions to generate random strings snd to manage temporal environment variables.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages  # Always prefer setuptools over distutilss
 
-VERSION = "1.1"
+VERSION = "1.1.0"
 
 with open("README.md") as readme:
     LONG_DESCRIPTION = readme.read()
